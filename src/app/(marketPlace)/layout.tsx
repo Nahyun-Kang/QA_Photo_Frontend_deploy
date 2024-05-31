e4499a820880f6478956c137faac29c271da1a7c
Nahyun <tarea202001@gmail.com>
2024-05-31 13:16:38 +0900

    Fix : 공통 헤더 로직 수정

--- src/app/(marketPlace)/layout.tsx
+++ src/app/(marketPlace)/layout.tsx
@@ -12,17 +12,31 @@ import styles from './page.module.scss'
 export default function MainLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname()
 
+  const commonHeader = (pathname: string) => {
+    if (pathname === '/') {
+      return null
+    } else if (PAGE_NAMES.hasOwnProperty(pathname)) {
+      return (
+        <div className={styles.commonHeaderContainer}>
+          <CommonHeader>{PAGE_NAMES[pathname]}</CommonHeader>
+        </div>
+      )
+    } else {
+      return (
+        <div className={styles.commonHeaderContainer}>
+          <CommonHeader>{'마켓플레이스'}</CommonHeader>
+        </div>
+      )
+    }
+  }
+
   return (
     <ReactHookFormProvider>
       <div className={styles.main}>
         <div>
           <MainHeader />
         </div>
-        {pathname !== '/' && (
-          <div className={styles.commonHeaderContainer}>
-            <CommonHeader>{PAGE_NAMES[pathname]}</CommonHeader>
-          </div>
-        )}
+        {commonHeader(pathname)}
         {children}
       </div>
     </ReactHookFormProvider>
