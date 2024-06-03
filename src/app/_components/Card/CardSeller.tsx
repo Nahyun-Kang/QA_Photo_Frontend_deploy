dd5c973946b5a758462bd86d2784ef7d91a767ad
Nahyun <tarea202001@gmail.com>
2024-06-03 18:20:06 +0900

    Feat: 판매 내리기 모달 구현

--- src/app/_components/Card/CardSeller.tsx
+++ src/app/_components/Card/CardSeller.tsx
@@ -5,6 +5,8 @@ import CardDetail from './CardDetailComponents'
 import styles from './CardSeller.module.scss'
 import ExchangeIcon from '/public/icons/exchange.svg'
 
+import ModalMain from '../Modal/Modal'
+import BasicModal from '../Modal/BasicModal'
 interface CardSellerProps {
   genre: string
   grade: GradeType
@@ -20,37 +22,49 @@ interface CardSellerProps {
 
 export default function CardSeller({ ...props }: CardSellerProps) {
   return (
-    <CardDetail>
-      <CardDetail.CardDetailInformation
-        genre={props.genre}
-        grade={props.grade}
-        maker={props.maker}
-      />
-      <CardDetail.BottomLine />
-      <CardDetail.Description description={props.description} />
-      <CardDetail.BottomLine />
-      <CardDetail.DetailQuantityContainer
-        price={props.price}
-        remainingQuantity={props.remainingQuantity}
-        totalQuantity={props.totalQuantity}
-      />
-      <div className={styles.exchange_title_container}>
-        <div className={styles.exchange_title_wrapper}>
-          <ExchangeIcon />
-          <div className={styles.exchange_title}>교환 희망 정보</div>
+    <>
+      {/* {
+        <ModalMain>
+          <BasicModal
+            title="포토카드 판매 내리기"
+            description={<>{`정말로 판매를 중단하시겠습니까?`}</>}
+            onClick={() => console.log()}
+            buttonName="판매 내리기"
+          />
+        </ModalMain>
+      } */}
+      <CardDetail>
+        <CardDetail.CardDetailInformation
+          genre={props.genre}
+          grade={props.grade}
+          maker={props.maker}
+        />
+        <CardDetail.BottomLine />
+        <CardDetail.Description description={props.description} />
+        <CardDetail.BottomLine />
+        <CardDetail.DetailQuantityContainer
+          price={props.price}
+          remainingQuantity={props.remainingQuantity}
+          totalQuantity={props.totalQuantity}
+        />
+        <div className={styles.exchange_title_container}>
+          <div className={styles.exchange_title_wrapper}>
+            <ExchangeIcon />
+            <div className={styles.exchange_title}>교환 희망 정보</div>
+          </div>
+          <div className={styles.section_line}></div>
         </div>
-        <div className={styles.section_line}></div>
-      </div>
-      <CardDetail.CardDetailInformation
-        genre={props.expectedGenre}
-        grade={props.expectedGrade}
-      />
-      <CardDetail.BottomLine />
-      <CardDetail.Description description={props.expectedContent} />
-      <div className={styles.buttonContainer}>
-        <Button>수정하기</Button>
-        <Button buttonStyle="secondary">판매 내리기</Button>
-      </div>
-    </CardDetail>
+        <CardDetail.CardDetailInformation
+          genre={props.expectedGenre}
+          grade={props.expectedGrade}
+        />
+        <CardDetail.BottomLine />
+        <CardDetail.Description description={props.expectedContent} />
+        <div className={styles.buttonContainer}>
+          <Button>수정하기</Button>
+          <Button buttonStyle="secondary">판매 내리기</Button>
+        </div>
+      </CardDetail>
+    </>
   )
 }
