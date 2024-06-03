f9b2c5b64af638bea8bf27d8ed0078799829a851
Nahyun <tarea202001@gmail.com>
2024-06-03 12:54:09 +0900

    Feat: 필터 틀 구현

---
+++ src/app/_components/Filter/index.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+export default function Filter() {
+  return (
+    <div>
+      <div>필터</div>
+      <div>
+        <div>
+          <span>등급</span>
+          <span>1</span>
+        </div>
+        <div>
+          <span>장르</span>
+          <span>2</span>
+        </div>
+        <div>
+          <span>매진여부</span>
+          <span>갯수</span>
+        </div>
+      </div>
+    </div>
+  )
+}
