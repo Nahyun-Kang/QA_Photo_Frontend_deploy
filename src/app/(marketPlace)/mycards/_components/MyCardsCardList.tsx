8062598add6a42630ea2a8a08c3ab4e161184f2d
Nahyun <tarea202001@gmail.com>
2024-06-03 15:11:49 +0900

    Feat: 나의 포토카드 판매 페이지 퍼블리싱

---
+++ src/app/(marketPlace)/mycards/_components/MyCardsCardList.tsx
@@ -0,0 +1,60 @@
+import styles from './MycardsCardList.module.scss'
+
+import SearchInput from '@/app/_components/SearchInput'
+import Dropdown from '@/app/_components/Dropdown'
+import {
+  GRADE_LIST,
+  GENRE_LIST,
+  SOLD_OUT_LIST,
+} from '@/app/_constants/listConstants'
+import { CARDS_LIST } from '@/app/(marketPlace)/CARD_LISTS'
+import Pagination from '@/app/_components/pagination'
+
+import Filter from '/public/icons/filter.svg'
+import CardForSale from '@/app/_components/Card/CardForSale'
+
+export default function MyCardsCardList() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.filterContainer}>
+        <div className={styles.filterWrapper}>
+          <button className={styles.button}>
+            <Filter width={20} height={20} />
+          </button>
+          <SearchInput />
+          <div className={styles.filters}>
+            <Dropdown attribute="등급" list={GRADE_LIST} />
+            <Dropdown attribute="장르" list={GENRE_LIST} />
+            <Dropdown attribute="판매방법" list={SOLD_OUT_LIST} />
+            <Dropdown attribute="매진여부" list={SOLD_OUT_LIST} />
+          </div>
+        </div>
+      </div>
+      <ul className={styles.ul}>
+        {CARDS_LIST?.map((el, idx) => {
+          return (
+            <li key={idx.toString()}>
+              <CardForSale
+                imageUrl={el.imageUrl}
+                nickName={el.nickName}
+                id={el.id}
+                userId={el.userId}
+                name={el.name}
+                price={el.price}
+                grade={el.grade}
+                genre={el.genre}
+                registeredQuantity={el.remainingQuantity}
+                createdDate={el.createdDate}
+                updatedDate={el.updatedDate}
+                method={el.method}
+              />
+            </li>
+          )
+        })}
+      </ul>
+      <div className={styles.paginationWrapper}>
+        <Pagination count={45} />
+      </div>
+    </section>
+  )
+}
--- src/app/(marketPlace)/mycards/page.tsx
+++ src/app/(marketPlace)/mycards/page.tsx
@@ -1,3 +1,26 @@
+import Link from 'next/link'
+
+import Title from '@/app/_components/Title'
+import Button from '@/app/_components/Button'
+
+import styles from './page.module.scss'
+import OwnedCards from '@/app/(marketPlace)/mygallery/_components/OwnedCards'
+
+import { MOCK_GRADELIST } from '@/app/(marketPlace)/mygallery/MOCK_DATA'
+import MyCardsCardList from './_components/MyCardsCardList'
+
 export default function MyCardsPage() {
-  return <div>나의 포토카드 판매 페이지</div>
+  return (
+    <main className={styles.main}>
+      <div className={styles.titleWrapper}>
+        <Title>
+          <div className={styles.titleContainer}>
+            <h2 className={styles.title}>나의 포토카드 판매하기</h2>
+          </div>
+        </Title>
+      </div>
+      <OwnedCards totalCount={20} gradeList={MOCK_GRADELIST} userName="유디" />
+      <MyCardsCardList />
+    </main>
+  )
 }
